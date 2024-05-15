bcff0dda2dd1784bdb81430abf4dbdbb17c95eb0
Fianda0 <fiandanesegiuseppe000@gmail.com>
2024-05-15 15:34:00 +0200

    Bonus

--- main.js
+++ main.js
@@ -4,22 +4,22 @@ createApp({
 
     data() {
         return {
-            lista: []
+            lista: [],
+            numero: ''
         }
     },
     methods: {
         chiamaMail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                console.log(result.data.response);
-                this.lista.push(result.data.response)
-            });
+            for (let index = 0; index < this.numero; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                    console.log(result.data.response);
+                    this.lista.push(result.data.response)
+                });
+            }
         }
     },
     mounted() {
-        for (let index = 0; index < 10; index++) {
-            this.chiamaMail()
-        }
-        console.log(this.lista);
+
     }
 
 }).mount('#app');
